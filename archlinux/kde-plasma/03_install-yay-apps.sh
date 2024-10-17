f19681f5a3074e5b92fb8518d1e3d05ac79c53e1
André Sá <andrepena.sa@gmail.com>
2024-10-17 12:49:32 -0300

    Alterações nos scripts

--- archlinux/kde-plasma/03_install-yay-apps.sh
+++ archlinux/kde-plasma/03_install-yay-apps.sh
@@ -7,20 +7,12 @@ yay -S --needed --noconfirm \
     google-chrome \
     syncthingtray \
     webapp-manager \
-    lens-bin \
-    postman-bin \
-    google-cloud-cli \
     android-tools \
-    forticlient-vpn \
     nordzy-cursors \
     slack-desktop \
     teamviewer \
     droidcam \
     droidcam-obs-plugin \
     v4l2loopback-dc-dkms \
-    google-cloud-cli-gke-gcloud-auth-plugin \
-    youtube-music-bin
-    #spotify
-    #fluent-icon-theme-git
-    #tela-circle-icon-theme-git \
-    #orchis-kde-theme-git \
+    youtube-music-bin \
+    spotify
--- archlinux/kde-plasma/04_configurations.sh
+++ archlinux/kde-plasma/04_configurations.sh
@@ -21,7 +21,7 @@ echo "Instalando Fontes..."
 wget -P ~/.local/share/fonts/ "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf" && \
     wget -P ~/.local/share/fonts/ "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf" && \
     wget -P ~/.local/share/fonts/ "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf" && \
-    wget -P ~/.local/share/fonts/ "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
+    wget -P ~/.local/share/fonts/ "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
 
 echo " "
 echo "----------------------"
